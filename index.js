7957eefe9bda69a76e5f4185b152d64bc8788fce
JJ <jj.vega86@gmail.com>
2021-01-26 16:45:35 -0500

    Registered cards route in index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const connectDB = require('./startup/db');
 const express = require('express');
 const app = express();
+const cards = require('./routes/cards');
 
 connectDB();
 
 app.use(express.json());
+app.use('/api/cards', cards);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
