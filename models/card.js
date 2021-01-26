48b86170a302d4037e2175a7bc349387fc777647
JJ <jj.vega86@gmail.com>
2021-01-26 15:57:37 -0500

    Added schema and model to card.js

---
+++ models/card.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+    title: { type: String, required: true, minlength: 2, maxLength: 30},
+    description: { type: String, required: true, minlength: 2, maxLength: 500},
+    cardNumber: { type: Number, required: true},
+    dateAdded: {type: Date, default: Date.now }
+});
+
+const Card = mongoose.model('Card', cardSchema);
+
+module.exports = Card;
