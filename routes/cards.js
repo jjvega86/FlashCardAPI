2c381e1f04b2d7a6f1d799527aa554ffb25a64a6
JJ <jj.vega86@gmail.com>
2021-01-26 16:43:13 -0500

    Installed nodemon, added imports and listening logic to index.js, added cards.js and imports to routes

---
+++ routes/cards.js
@@ -0,0 +1,7 @@
+const Card = require('../models/card');
+const express = require('express');
+const router = express.Router();
+
+// All endpoints and route handlers go here
+
+module.exports = router;
