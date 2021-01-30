222c006cb1e5243087813d3958ff50f10d83ba6b
JJ <jj.vega86@gmail.com>
2021-01-30 15:15:13 -0500

    Added GET routes for all cards and a single card

--- routes/cards.js
+++ routes/cards.js
@@ -4,6 +4,29 @@ const router = express.Router();
 
 // All endpoints and route handlers go here
 
+router.get('/', async (req, res) => {
+    try{
+        const cards = await Card.find();
+        return res.send(cards);
+    } catch(ex){
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try{
+        const card = Card.findById(req.params.id);
+
+        if(!card)
+            return res.status(400).send(`The product with id ${req.params.id} does not exist`);
+
+        return res.send(card);
+    } catch(ex){
+        return res.status(500).send(`Internal Server Error: ${ex}`)
+    }
+
+});
+
 router.post('/', async (req, res) => {
     try{
 
